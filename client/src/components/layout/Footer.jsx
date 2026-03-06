9b354fedf087948a776affce36f52f25604fd1f8
Mu Fazle Elahi Mahi <fazle.cse.20230104053@aust.edu>
2026-03-06 21:46:39 +0600

    updated

--- client/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="w-full bg-white dark:bg-gray-800 text-center py-4 text-gray-400 text-sm mt-auto">
-    &copy; {new Date().getFullYear()} University Management System
-  </footer>
-);
-
-export default Footer;
